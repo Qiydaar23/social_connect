c7e50ee0bc226fad58e31ef1221a42b399044e15
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-03 00:08:31 -0400

    touch up

--- users/models.py
+++ users/models.py
@@ -10,13 +10,13 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
     
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         
         
-        # img = Image.open(self.image.path)
+        img = Image.open(self.image.path)
         
-        # if img.height > 300 or img.width > 300:
-        #     output_size = (250, 250)
-        #     img.thumbnail(output_size)
-        #     img.save(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output_size = (250, 250)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
