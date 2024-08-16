268dd2ed57dbe58bb03332d0cd46789ff591bc8c
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-15 21:37:06 -0400

    updated python/venv

--- social_connects/settings.py
+++ social_connects/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y1vw2g6ydfknh*-m&+f$6@05-q+1=3ym+d&y)oh^1me6vzf&#f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost','http://sconnectsproject-env.eba-6g63qftx.us-east-2.elasticbeanstalk.com/']
 
 
 # Application definition
