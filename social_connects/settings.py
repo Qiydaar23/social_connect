7d9e0f33a3f47421c50fb19cd8af96c5bfbd71e0
Qiydaarwilliams23@gmail.com <Qiydaarwilliams23@gmail.com>
2024-08-21 16:51:42 -0400

    seetings reverted

--- social_connects/settings.py
+++ social_connects/settings.py
@@ -12,12 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
-DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
-AWS_S3_SESSION_PROFILE = 'eb-cli'
-AWS_STORAGE_BUCKET_NAME = 'socialconnectsbucket'
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
-AWS_s3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+# DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storage.backends.s3boto3.S3Boto3Storage'
+# AWS_S3_SESSION_PROFILE = 'eb-cli'
+# AWS_STORAGE_BUCKET_NAME = 'socialconnectsbucket'
+# AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
+# AWS_s3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -146,6 +146,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+# MEDIA_URL = AWS_S3_CUSTOM_DOMAIN + '/media/'
+# MEDIA_ROOT = AWS_S3_CUSTOM_DOMAIN
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
